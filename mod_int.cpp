7921a9a50a38d2500ac84928b908f72713591695
furuya1223 <kouki23.bass@gmail.com>
2020-05-04 12:30:12 +0900

    add mod int

--- mod_int.cpp
+++ mod_int.cpp
@@ -1,50 +1,50 @@
 #include "header.hpp"
 
-template <std::uint_fast64_t Mod>
-class ModInt {
-    using u64 = std::uint_fast64_t;
+template <uint_fast64_t Mod>
+class mint {
+    using u64 = uint_fast64_t;
     u64 value;
 
   public:
-    constexpr ModInt(long long value = 0) noexcept : value(value % Mod) {}
+    constexpr mint(long long value = 0) noexcept : value(value % Mod) {}
 
-    constexpr u64 &get() noexcept {
+    u64 &get() noexcept {
         return value;
     }
     constexpr const u64 &get() const noexcept {
         return value;
     }
-    constexpr ModInt operator+(const ModInt rhs) const noexcept {
-        return ModInt(*this) += rhs;
+    constexpr mint operator+(const mint &rhs) const noexcept {
+        return mint(*this) += rhs;
     }
-    constexpr ModInt operator-(const ModInt rhs) const noexcept {
-        return ModInt(*this) -= rhs;
+    constexpr mint operator-(const mint &rhs) const noexcept {
+        return mint(*this) -= rhs;
     }
-    constexpr ModInt operator*(const ModInt rhs) const noexcept {
-        return ModInt(*this) *= rhs;
+    constexpr mint operator*(const mint &rhs) const noexcept {
+        return mint(*this) *= rhs;
     }
-    constexpr ModInt operator/(const ModInt rhs) const noexcept {
-        return ModInt(*this) /= rhs;
+    constexpr mint operator/(const mint &rhs) const noexcept {
+        return mint(*this) /= rhs;
     }
-    constexpr ModInt &operator+=(const ModInt rhs) noexcept {
+    mint &operator+=(const mint &rhs) noexcept {
         value += rhs.value;
         if (value >= Mod) {
             value -= Mod;
         }
         return *this;
     }
-    constexpr ModInt &operator-=(const ModInt rhs) noexcept {
+    mint &operator-=(const mint &rhs) noexcept {
         if (value < rhs.value) {
             value += Mod;
         }
         value -= rhs.value;
         return *this;
     }
-    constexpr ModInt &operator*=(const ModInt rhs) noexcept {
+    mint &operator*=(const mint &rhs) noexcept {
         value = value * rhs.value % Mod;
         return *this;
     }
-    constexpr ModInt &operator/=(ModInt rhs) noexcept {
+    mint &operator/=(mint rhs) noexcept {
         u64 exp = Mod - 2;
         while (exp) {
             if (exp % 2 == 1) {
@@ -55,8 +55,8 @@ class ModInt {
         }
         return *this;
     }
-    constexpr ModInt pow(u64 n) const noexcept {
-        ModInt ret(1), p(*this);
+    mint pow(u64 n) const noexcept {
+        mint ret(1), p(*this);
         while (n > 0) {
             if (n & 1) ret *= p;
             p *= p;
@@ -65,29 +65,31 @@ class ModInt {
         return ret;
     }
 };
-template <std::uint_fast64_t Mod>
-ostream &operator<<(ostream &os, ModInt<Mod> &modint) {
+template <class Char, class Traits, uint_fast64_t Mod>
+basic_ostream<Char, Traits> &operator<<(basic_ostream<Char, Traits> &os,
+                                        const mint<Mod> &modint) {
     os << modint.get();
     return os;
 }
-template <std::uint_fast64_t Mod>
-istream &operator>>(istream &is, ModInt<Mod> &modint) {
+template <class Char, class Traits, uint_fast64_t Mod>
+std::basic_istream<Char, Traits> &operator>>(basic_istream<Char, Traits> &is,
+                                             mint<Mod> &modint) {
     is >> modint.get();
     return is;
 }
-template <std::uint_fast64_t Mod>
-constexpr bool operator==(const ModInt<Mod> &lhs, const ModInt<Mod> &rhs) {
+template <uint_fast64_t Mod>
+constexpr bool operator==(const mint<Mod> &lhs, const mint<Mod> &rhs) {
     return lhs.get() == rhs.get();
 }
-template <std::uint_fast64_t Mod>
-constexpr bool operator!=(const ModInt<Mod> &lhs, const ModInt<Mod> &rhs) {
+template <uint_fast64_t Mod>
+constexpr bool operator!=(const mint<Mod> &lhs, const mint<Mod> &rhs) {
     return lhs != rhs;
 }
-template <std::uint_fast64_t Mod>
-constexpr bool operator==(const long long lhs, const ModInt<Mod> &rhs) {
+template <uint_fast64_t Mod>
+constexpr bool operator==(const long long lhs, const mint<Mod> &rhs) {
     return lhs == rhs.get();
 }
-template <std::uint_fast64_t Mod>
-constexpr bool operator!=(const long long lhs, const ModInt<Mod> &rhs) {
+template <uint_fast64_t Mod>
+constexpr bool operator!=(const long long lhs, const mint<Mod> &rhs) {
     return lhs != rhs.get();
 }
