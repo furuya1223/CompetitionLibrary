2c4cb4853b310e0636a695ee531d9a2c5234bda9
furuya1223 <kouki23.bass@gmail.com>
2020-05-04 12:30:44 +0900

    add union-find tree

--- union_find.cpp
+++ union_find.cpp
@@ -1,62 +1,55 @@
-/*
+#include "header.hpp"
+
 class UnionFind {
-public:
-	vector<int> par, rank, _size;
-	int _num;
+  public:
+    vector<int> par, rank, size_;
+    int num_;
 
-	UnionFind(int n) {
-		par = vector<int>(n);
-		rank = vector<int>(n);
-		_size = vector<int>(n);
-		num = n;
-		for (int i = 0; i < n; i++) {
-			par[i] = i;
-			rank[i] = 0;
-			_size[i] = 1;
-		}
-	}
+    UnionFind(int n) {
+        par = vector<int>(n);
+        rank = vector<int>(n);
+        size_ = vector<int>(n);
+        num_ = n;
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            rank[i] = 0;
+            size_[i] = 1;
+        }
+    }
 
-	// 木の根を求める
-	int find(int x) {
-		if (par[x] == x) {
-			return x;
-		}
-		else {
-			return par[x] = find(par[x]);
-		}
-	}
+    int find(int x) {
+        if (par[x] == x) {
+            return x;
+        } else {
+            return par[x] = find(par[x]);
+        }
+    }
 
-	// xとyの属する集合を併合
-	void unite(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y)return;
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
 
-		num--;
-		if (rank[x] < rank[y]) {
-			par[x] = y;
-			_size[y] += _size[x];
-		}
-		else {
-			par[y] = x;
-			_size[x] += _size[y];
-			if (rank[x] == rank[y])rank[x]++;
-		}
-	}
+        num_--;
+        if (rank[x] < rank[y]) {
+            par[x] = y;
+            size_[y] += size_[x];
+        } else {
+            par[y] = x;
+            size_[x] += size_[y];
+            if (rank[x] == rank[y]) rank[x]++;
+        }
+    }
 
-	// xとyが同じ集合に属するか否か
-	bool same(int x, int y) {
-		return find(x) == find(y);
-	}
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
 
-	// xの属する集合のサイズ
-	int size(int x) {
-		return _size[find(x)];
-	}
+    int size(int x) {
+        return size_[find(x)];
+    }
 
-	// 集合の個数
-	int num(){
-		return _num;
-	}
+    int num() {
+        return num_;
+    }
 };
-*/
